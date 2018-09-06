28d3e6e3c62d80ec0e8489f7fa6b34025569f075
Evan New-Schmidt <evan@new-schmidt.com>
2018-09-06 13:18:44 -0400

    Initial test of React Player
    Websockets were throwing errors so I commented them out temporarily.

--- src/video-info-panel.jsx
+++ src/video-info-panel.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SocketIO from 'socket.io-client';
+import ReactPlayer from 'react-player';
 
 export default class VideoInfoPanel extends React.Component {
     // TODO: Add websockets
     constructor(props) {
         super(props)
-        this.state = { name: "Test", url: "test2" };
+        this.state = { name: "drifto", url: "https://www.youtube.com/watch?v=yothf5A2Mgk" };
         // TODO: Fix the server URI
-        this.client = new SocketIO('http://localhost:9091');
-        this.client.on('update', msg => this.handleUpdate(msg));
+        // this.client = new SocketIO('http://localhost:9091');
+        // this.client.on('update', msg => this.handleUpdate(msg));
     }
 
     handleUpdate = (msg) => {
         console.log(msg);
         this.setState({ name: msg })
-    }    
+    }
 
     render() {
         return (
         <div>
-            {this.state.name}    
+            <h1>{this.state.name}</h1>
+            <ReactPlayer url={this.state.url} playing />
         </div>);
     }
 
-    
-} 
+
+}
